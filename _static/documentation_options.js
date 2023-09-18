1af9640ce0d55694f2b8b9646f0dfd436efb5fd5
janash <janash@vt.edu>
2023-09-18 18:36:57 +0000

    Deploying to test-docs from @ janash/QCFractal@499a156797ad0275e9ef37be3a4ee98ed95209c2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.50.post14+g199fc78b',
+    VERSION: '0.50.post15+g499a1567',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
